bfaecef25fdebb42b3cf4151f081f792dbe380c5
jhovaniM12 <jhovanimoreno12@gmail.com>
2026-06-17 18:31:35 -0500

    D  packages/web/api/[[...route]].ts M  packages/web/vercel.json A  packages/web/src/app/api/[[...route]]/route.ts

---
+++ packages/web/src/app/api/[[...route]]/route.ts
@@ -0,0 +1,11 @@
+export const runtime = "nodejs";
+
+import handler from "../../../../../functions/dist/vercel.js";
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const PATCH = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;
+export const HEAD = handler;
